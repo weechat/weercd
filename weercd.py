bf8a7da98325026993dd0e3b060e00fe47e91070
Sébastien Helleu <flashcode@flashtux.org>
2016-01-18 07:50:26 +0100

    Update copyright dates

--- weercd.py
+++ weercd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2011-2015 Sébastien Helleu <flashcode@flashtux.org>
+# Copyright (C) 2011-2016 Sébastien Helleu <flashcode@flashtux.org>
 #
 # This file is part of weercd, the WeeChat IRC testing server.
 #
